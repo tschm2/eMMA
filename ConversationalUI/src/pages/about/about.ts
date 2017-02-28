a3af56253e843162a430da2007a9b4ece25f1f83
tschm2 <emautsc2@gmail.com>
2017-02-28 19:58:35 +0100

    asd
    asd

--- ConversationalUI/src/pages/about/about.ts
+++ ConversationalUI/src/pages/about/about.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
+
+
 
 @Component({
   selector: 'page-about',
@@ -8,8 +10,117 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+
   constructor(public navCtrl: NavController) {
 
-  }
-
 }
+
+
+  showThis(elem) {
+    BarcodeScanner.scan().then((barcodeData) => {
+      console.log(barcodeData);
+
+
+      }, (err) => {
+        // An error occurred
+      });
+
+      class Base64 {
+        private PADCHAR: string = '=';
+        private ALPHA: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+        private getByte(s: string, i: number): number {
+          const x = s.charCodeAt(i);
+          return x;
+        }
+
+        private getByte64(s: string, i: number): number {
+          const idx = this.ALPHA.indexOf(s.charAt(i));
+          return idx;
+        }
+
+        public decode (s: string): string {
+          let pads = 0,
+            i, b10, imax = s.length,
+            x = [];
+
+          s = String(s);
+
+          if (imax === 0) {
+            return s;
+          }
+
+          if (s.charAt(imax - 1) === this.PADCHAR) {
+            pads = 1;
+            if (s.charAt(imax - 2) === this.PADCHAR) {
+              pads = 2;
+            }
+            imax -= 4;
+          }
+
+          for (i = 0; i < imax; i += 4) {
+            b10 = (this.getByte64(s, i) << 18) | (this.getByte64(s, i + 1) << 12) | (this.getByte64(s, i + 2) << 6) | this.getByte64(s, i + 3);
+            x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 255, b10 & 255));
+          }
+
+          switch (pads) {
+            case 1:
+              b10 = (this.getByte64(s, i) << 18) | (this.getByte64(s, i + 1) << 12) | (this.getByte64(s, i + 2) << 6);
+              x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 255));
+              break;
+            case 2:
+              b10 = (this.getByte64(s, i) << 18) | (this.getByte64(s, i + 1) << 12);
+              x.push(String.fromCharCode(b10 >> 16));
+              break;
+          }
+
+          return x.join('');
+        }
+
+        public encode(s: string): string {
+          s = String(s);
+
+          let i, b10, x = [],
+            imax = s.length - s.length % 3;
+
+          if (s.length === 0) {
+            return s;
+          }
+
+          for (i = 0; i < imax; i += 3) {
+            b10 = (this.getByte(s, i) << 16) | (this.getByte(s, i + 1) << 8) | this.getByte(s, i + 2);
+            x.push(this.ALPHA.charAt(b10 >> 18));
+            x.push(this.ALPHA.charAt((b10 >> 12) & 63));
+            x.push(this.ALPHA.charAt((b10 >> 6) & 63));
+            x.push(this.ALPHA.charAt(b10 & 63));
+          }
+
+          switch (s.length - imax) {
+            case 1:
+              b10 = this.getByte(s, i) << 16;
+              x.push(this.ALPHA.charAt(b10 >> 18) + this.ALPHA.charAt((b10 >> 12) & 63) + this.PADCHAR + this.PADCHAR);
+              break;
+            case 2:
+              b10 = (this.getByte(s, i) << 16) | (this.getByte(s, i + 1) << 8);
+              x.push(this.ALPHA.charAt(b10 >> 18) + this.ALPHA.charAt((b10 >> 12) & 63) + this.ALPHA.charAt((b10 >> 6) & 63) + this.PADCHAR);
+              break;
+          }
+
+          return x.join('');
+        }
+      }
+
+      let test = new Base64();
+      var dada = "H4sIAAAAAAAEAK1UzW7aQBB+lY2vsaNd24DNLQmhRQktIrSRWuWwtge8wl6j9bpNQLxNHqO3vFhn7TglAXKqhPF4Z3bmm29+NtZ5pVOrb/W6lFHqudQPOz3LtgYaD13Kug6jDgtmLOgzr+95p9TtU4oGowQNIt+bh91o7vjzwHV8CCMn5FHghDGN58E86PDQOBtDMntcgdVntSxinoPUpdX/ubHOV6uRLLVCb5LHKUkgJ1dlCbKN4fshY14TsvHi2dakaG4P8J/Z1Mbn3oAeqiJvgdMQf3Uqs2LnjIXWFm0nqoyji0dUDNQZ+VJoMlRcrm3zqkDOIUvw7rTgaDH5iuJsuZiWEr9GqFxqURiE36QwRN3Orq2tvWkAuwGjXtf/GDA9BNhzKDsA7lytNfnMZWmT8fMfGacgU141FB0CeJFhTYs4TVQVL4+AZKHb890PWKXHWP1PIC/TIoNSgxISjZag9oHutkaliDjLz2xipKRS2CnC6CoBSgOZgCoLCfKk7RqXscD3jqfnHqxBx2EMMzyQ3tUDz1cZkGuuMpvcPT/JhXjNbDR+Qz+oKhWLSi7+pTS+eZ9RBIJcQMLVHK1wCAscjHpAavg4AWLxW8RLyHZyYPslOthHL40/klNAa2ayeUU3FBDtku3sARMlWVck5w9nxMdXRvTz0yITyDgjMx5loJFwEFJCmtfl3UPv97oe83pH2P8QKyKdNWZFY0a392+r8X5THWqu2xihqTW8H1HjiGuBy8fqb6yLesmx0PfqsjM0vhTaxLjjWWIy1k0Rh19wYZkgoGvqPoFMUDBob15U46psdDd4A1vmqll7JszgptRjMLDqM97Q8FrRBp9rW995hjYBqwvSqN1WzVo1C2idxjSuvYwa1qf40en17tvxdlvBawW/FTqt0G2vMSR4i8SkOEAY4dRnpBcS5nrE75CuofhWKwDdtPYCu4TjfibMbLgfYoXHoRviUkV/+dLkDoq8sExEqcmvIidm6y+5WZrlKuOSRLAAYRjTJ9b2LyrDpWuFBgAA";
+      var dada = test.decode(dada);
+      console.log(dada);
+
+
+
+        var itemToShow = document.getElementById(elem);
+        console.log(itemToShow);
+
+
+    }
+
+    }
